45a84e4aa3968e423fe6ecee091dbd8e6587b860
Ricardo Guerra <RikkuG491@gmail.com>
2024-07-02 11:04:09 -0400

    Created content and starter code for Lab - Callback Functions

---
+++ index.js
@@ -0,0 +1,5 @@
+const petNames = ["Fido", "Kitty", "Hammy", "Polly", "Angel", "Fluffy", "Goldie"];
+
+// Write your solution code for Task # 1 here!
+
+// Write your solution code for Task # 2 here!
